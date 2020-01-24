a7c689bc1fc12983e00d14940eca68574bac5fef
Сергей <41532402+Svift87@users.noreply.github.com>
2020-01-24 17:02:55 +0700

    app

--- src/main.js
+++ src/main.js
@@ -1,13 +1,43 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import dateFilter from './filters/date.filter'
+import messagePlagin from './coman/message.plagin'
+import 'materialize-css/dist/js/materialize.min'
+
+import firebase from 'firebase/app'
+import 'firebase/auth';
+import 'firebase/database';
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+Vue.use(messagePlagin)
+Vue.use(Vuelidate)
+Vue.filter('date', dateFilter)
+
+// Your web app's Firebase configuration
+firebase.initializeApp({
+  apiKey: "AIzaSyDpR-iUwF15CaYTFlbXizrG_mzfRO9pHbg",
+  authDomain: "vuecrm-9f88c.firebaseapp.com",
+  databaseURL: "https://vuecrm-9f88c.firebaseio.com",
+  projectId: "vuecrm-9f88c",
+  storageBucket: "vuecrm-9f88c.appspot.com",
+  messagingSenderId: "956185361960",
+  appId: "1:956185361960:web:12c6ab5c3981e4fe757f1d"
+})
+
+let app 
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }  
+})
+// Initialize Firebase
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -8,19 +7,80 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/Home.vue')
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/register',
+    name: 'register',
+    meta: {
+      layout: 'empty'
+    },
+    component: () => import('../views/Register.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    meta: {
+      layout: 'empty'
+    },
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/Detail-record',
+    name: 'Detail-record',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/Detail-record.vue')
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/Categories.vue')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/History.vue')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/Planning.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/Record.vue')
   }
 ]
 
 const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes
 })
 
